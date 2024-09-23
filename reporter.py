0961eef1d94d510c704cd56eebb7888b1af270b3
willdrug <whydafuqdoienedthis@gmail.com>
2024-09-23 17:41:03 +0200

    test update to fix reporting

--- reporter.py
+++ reporter.py
@@ -2,11 +2,10 @@
 
 from os import getenv
 from blog_web import app, tc
-if not getenv('LOCAL'):
-    print(f'Starting reporter.')
-    tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new', tags=['Personal', 'Article'])
-    tc.discover.start_reporting()
 
 if __name__ == '__main__':
-    # used only for debugging with LOCAL expected.
-    app.run('0.0.0.0', '8888', debug=True)
+    if not getenv('LOCAL'):
+        print(f'Starting reporter.')
+        tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new',
+                               tags=['Personal', 'Article'])
+        tc.discover.start_reporting()
