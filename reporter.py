adefa125b08a26d2df1ca30dfd624571b1716e67
willdrug <whydafuqdoienedthis@gmail.com>
2024-09-23 17:45:36 +0200

    restores -c

--- reporter.py
+++ reporter.py
@@ -3,9 +3,9 @@
 from os import getenv
 from blog_web import app, tc
 
-if __name__ == '__main__':
-    if not getenv('LOCAL'):
-        print(f'Starting reporter.')
-        tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new',
-                               tags=['Personal', 'Article'])
-        tc.discover.start_reporting()
+
+if not getenv('LOCAL'):
+    print(f'Starting reporter.')
+    tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new',
+                           tags=['Personal', 'Article'])
+    tc.discover.start_reporting()
