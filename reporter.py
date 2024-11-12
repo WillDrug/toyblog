13bd2163f8374b39167f45d9047b5221c547b724
willdrug <whydafuqdoienedthis@gmail.com>
2024-11-12 21:04:22 +0100

    changed reporter to a hook

--- reporter.py
+++ reporter.py
@@ -4,8 +4,9 @@ from os import getenv
 from blog_web import app, tc
 
 
-if not getenv('LOCAL'):
-    print(f'Starting reporter.')
-    tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new',
-                           tags=['Personal', 'Article'])
-    tc.discover.start_reporting()
+def on_starting(server):
+    if not getenv('LOCAL'):
+        print(f'Starting reporter.')
+        tc.discover.set_params(tc.name, 'Little Blog', 'An archive of old and a storage for new',
+                               tags=['Personal', 'Article'])
+        tc.discover.start_reporting()
